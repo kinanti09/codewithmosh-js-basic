130b3e8fbc7378a000cb3c46363c3810077f5fde
kinanti09 <kinantianjang09@gmail.com>
2019-01-19 18:43:13 +0700

    Non-booleans

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-// Logical OR (||)
-// Mengembalikan BENAR jika kedua operan BENAR
+let userColor = undefined;
+let defaultColor = 'pink';
+let currentColor = userColor || defaultColor;
 
-
-let penghasilan = false;
-let kredit = false;
-let syaratPinjaman = penghasilan || kredit;
-console.log('Berhak', syaratPinjaman);
-
-// NOT (!)
-let aplikasiDitolak = !syaratPinjaman;
-console.log('Aplikasi Ditolak', aplikasiDitolak);
+console.log(currentColor);
