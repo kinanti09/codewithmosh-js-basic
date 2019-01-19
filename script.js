97f8c54fd6a9498ee9b4f7915d99b4eb2505dee4
kinanti09 <kinantianjang09@gmail.com>
2019-01-19 19:39:16 +0700

    operators-precendence

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-// Read, Write, Execute
-// 00000100
-// 00000010
-// 00000001
 
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
+//calculator
+let x = (2 + 3) * 4;
 
-let myPermission = 0;
-myPermission = myPermission | readPermission | writePermission;
+console.log(x);
 
-
-let message =
-    (myPermission & readPermission) ? 'yes' : 'no';
-
-console.log(message);
