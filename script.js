b6b005b79beeea7c7936e9e6082770e85ffd71a4
kinanti09 <kinantianjang09@gmail.com>
2019-01-19 19:56:31 +0700

    if...else

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Hour
+// If hour is between 6am and 12pm : Good Morning!
+// If it is between 12pm and 6pm : Good afternoon!
+// Otherwise : Good evening!
 
-//calculator
-let x = (2 + 3) * 4;
+let hour = 20;
 
-console.log(x);
-
+if (hour >= 6 && hour < 12)
+    console.log('Good morning');
+else if (hour >= 12 && hour < 18)
+    console.log('Good afternoon');
+else
+    console.log('Good evening');
