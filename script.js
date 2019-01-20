ccf2e5e5a1b3d1e3e64c7d89799e44cb33c16b09
kinanti09 <kinantianjang09@gmail.com>
2019-01-20 08:38:30 +0700

    do...while

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
-for (let i = 0; i < 10; i++) {
-    if (i % 2 !== 0) 
-        console.log(i);
-}
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 !== 0) 
+//         console.log(i);
+// }
 
-let i = 0;
-while (i <= 5) {
+// let i = 9;
+// while (i <= 5) {
+//     if (i % 2 !== 0)
+//     console.log(i);
+//     i++;
+// }
+
+// do-while
+let i = 7;
+do {
     if (i % 2 !== 0)
-    console.log(i);
-    i++;
-}
+        console.log(i);
+        i++;
+
+} while(i <= 5);
 
