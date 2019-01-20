d02babdd6522b419ebedc513c5c32d29a67ccc4c
kinanti09 <kinantianjang09@gmail.com>
2019-01-20 08:29:13 +0700

    while

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
-// for (let i = 1; i < 10; i++) {
-//     console.log('Hello World', i);
-// }
-
-
 for (let i = 0; i < 10; i++) {
     if (i % 2 !== 0) 
         console.log(i);
+}
+
+let i = 0;
+while (i <= 5) {
+    if (i % 2 !== 0)
+    console.log(i);
+    i++;
 }
 
